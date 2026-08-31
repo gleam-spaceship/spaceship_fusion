9db0a5c34b2102dc67ac624badb35cfcfc54fdec
wai-lin <dev.wailin@gmail.com>
2026-08-31 21:54:32 +0700

    feat: add Erlang FFI for shell commands

---
+++ src/dev_ffi.erl
@@ -0,0 +1,39 @@
+-module(dev_ffi).
+-export([run_command/2, run_command_in/3]).
+
+run_command(Cmd, Args) ->
+    run_command_in(Cmd, Args, ".").
+
+run_command_in(Cmd, Args, Cwd) ->
+    ArgsList = [to_list(A) || A <- Args],
+    CwdStr = to_list(Cwd),
+    CmdStr = to_list(Cmd),
+    FilteredArgs = ["\"" ++ A ++ "\"" || A <- ArgsList, A =/= ""],
+    FullCmd = "cd " ++ CwdStr ++ " && " ++ CmdStr ++ " " ++ string:join(FilteredArgs, " "),
+    try
+        %% os:cmd/1 returns command output, not its exit status. Append a
+        %% marker so failed builds are not reported as successful.
+        Output = os:cmd(FullCmd ++ "; printf '\\n__FUSION_STATUS:%s\\n' \"$?\""),
+        command_result(Output, FullCmd)
+    catch
+        error:Reason ->
+            Msg = iolist_to_binary(io_lib:format("~p", [Reason])),
+            {error, iolist_to_binary([<<"Failed: ">>, iolist_to_binary(FullCmd), <<" - ">>, Msg])}
+    end.
+
+command_result(Output, FullCmd) ->
+    Marker = "__FUSION_STATUS:",
+    case string:split(Output, Marker, all) of
+        [CommandOutput, StatusWithRest] ->
+            Status = hd(string:split(StatusWithRest, "\n", all)),
+            case Status of
+                "0" -> {ok, unicode:characters_to_binary(CommandOutput)};
+                _ -> {error, unicode:characters_to_binary(["Command failed: ", FullCmd, "\n", CommandOutput])}
+            end;
+        _ ->
+            {error, unicode:characters_to_binary(["Could not determine command status: ", FullCmd])}
+    end.
+
+to_list(Data) when is_binary(Data) -> binary_to_list(Data);
+to_list(Data) when is_list(Data) -> Data;
+to_list(Data) -> lists:flatten(io_lib:format("~p", [Data])).
